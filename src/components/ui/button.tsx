48d83f161eea8f61b83c58d3641c768755c5bd66
Sanshiroh SAKAI <sansyo@gmail.com>
2026-04-03 07:51:24 +0900

    fix: biome.json に tailwindDirectives を有効化し Biome CI エラーを解消

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,9 +1,8 @@
-import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import type { VariantProps } from 'class-variance-authority'
-
-import { cn } from '@/lib/utils'
+import * as React from 'react'
 import { buttonVariants } from '@/lib/button-variants'
+import { cn } from '@/lib/utils'
 
 export { buttonVariants }
 
